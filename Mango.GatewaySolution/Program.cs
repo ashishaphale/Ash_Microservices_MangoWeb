5215554d6d6df60b4ca1bdfc504778a8287315a6
Ashish Aphale <aphale.ashish@gmail.com>
2024-06-03 19:43:39 +0530

    Added Ocelet Functionality.

---
+++ Mango.GatewaySolution/Program.cs
@@ -0,0 +1,13 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Mango.GatewaySolution.Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.AddAppAuthentication();
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+app.UseOcelot().GetAwaiter().GetResult();
+app.Run();
